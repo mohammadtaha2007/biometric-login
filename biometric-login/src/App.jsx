a9920dd66422d61567d5a6a2c83e0032793f0c92
MohammadTaha Rahimi <mohammadtahar2007@gmail.com>
2026-06-11 18:42:51 +0330

    CreateLoginHandler

--- biometric-login/src/App.jsx
+++ biometric-login/src/App.jsx
@@ -9,4 +9,15 @@ export default function App() {
 
   const USER = "admin";
   const PASSWORD = "1234";
+
+  const loginHandler = (e) => {
+    e.preventDefault();
+
+    if (username === USER && password === PASSWORD) {
+      setIsLoggedIn(true);
+      setError("");
+    } else {
+      setError("UserName Of Password Is Not Fetch:(");
+    }
+  };
 }
